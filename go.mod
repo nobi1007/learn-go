f98266b3b2264f48be8711a57f6820fdf06ef3df
nobi1007 <mittalshyam1007@gmail.com>
2024-06-26 10:44:03 +0530

    greetings module init

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/nobi1007/learn-go/greetings
+
+go 1.21.6
---
+++ greetings.go
@@ -0,0 +1,10 @@
+package greetings
+
+import (
+	"fmt"
+)
+
+func Hello(name string) string {
+	msg := fmt.Sprintf("Hello %v, Welcome", name)
+	return msg
+}
